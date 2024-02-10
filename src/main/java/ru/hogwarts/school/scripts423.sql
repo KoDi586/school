36894f2ba3c92daea4b61cddbc5102f5c2db337e
hirof <hirofool502@gmail.com>
2024-02-10 20:45:34 +0300

    423 work version

--- src/main/java/ru/hogwarts/school/scripts423.sql
+++ src/main/java/ru/hogwarts/school/scripts423.sql
@@ -1,2 +1,9 @@
 SELECT id, age, name, avatar_id, faculty_id
 FROM students;
+
+select students.name, students.age, faculties.name
+from students inner join faculties on students.faculty_id = faculties.id
+
+select students.name, students.age, faculties.name
+from students inner join faculties on students.faculty_id = faculties.id
+where students.avatar_id is not null
