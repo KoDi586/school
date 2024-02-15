153274b5fd187e9539f6bb793e35043cc740ed89
hirof <hirofool502@gmail.com>
2024-02-15 16:53:10 +0300

    finish training with indexes

---
+++ src/main/resources/liquibase/scripts/first-practice-index.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset kodi:1
+CREATE INDEX students_name_index ON students (name)
+
+
+-- changeset kodi:2
+CREATE INDEX faculties_nameAndColor_index ON faculties (name, color)
+
